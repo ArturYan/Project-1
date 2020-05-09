65a107c6e92f7c58288e3057fdb6adcabe3c26ce
Nicole Walsh <nmwalsh92@gmail.com>
2020-05-08 22:37:48 -0700

    About Us and Contact Page updated

---
+++ contactscript.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+//     M.updateTextFields();
+//   });
+
+//   $('#textarea1').val('New Text');
+//   M.textareaAutoResize($('#textarea1'));
+
+
+$("#button").on("click", function(){
+    var first = $("#first_name").val();
+    var last = $("#last_name").val();
+    var email = $("#email").val();
+    var response = $("#textarea").val();
+    localStorage.setItem("First Name", JSON.stringify(first));
+    localStorage.setItem("Last Name", JSON.stringify(last));
+    localStorage.setItem("Email", JSON.stringify(email));
+    localStorage.setItem("Response", JSON.stringify(response));
+    });
+    
+    
+    // when i click the save button - first name, last name, email and text are saved to local storage.
+    // Then message contents are cleared out
