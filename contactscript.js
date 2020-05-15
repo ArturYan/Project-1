164551288d549511b0516189b7c67dcca1f908fa
Nicole Walsh <nmwalsh92@gmail.com>
2020-05-14 20:05:10 -0700

    updated contact page html and js

--- contactscript.js
+++ contactscript.js
@@ -1,9 +1,4 @@
-// $(document).ready(function() {
-//     M.updateTextFields();
-//   });
 
-//   $('#textarea1').val('New Text');
-//   M.textareaAutoResize($('#textarea1'));
 
 
 $("#button").on("click", function(){
@@ -15,9 +10,14 @@ $("#button").on("click", function(){
     localStorage.setItem("Last Name", JSON.stringify(last));
     localStorage.setItem("Email", JSON.stringify(email));
     localStorage.setItem("Response", JSON.stringify(response));
+    $('#first_name').val('').removeClass('valid');
+    $('#last_name').val('').removeClass('valid');
+    $('#email').val('').removeClass('valid');
+    $('#textarea').val('').removeClass('valid');
+      M.updateTextFields();
     });
-    
-    
+ 
+
     // when i click the save button - first name, last name, email and text are saved to local storage.
     // Then message contents are cleared out
 
@@ -41,4 +41,49 @@ $("#button").on("click", function(){
     // END OF OPEN LAYER SCRIPT
 
     // Map API
-    // AJAX
+   
+   
+   
+   
+   
+    // weather application
+
+
+// This is our API key
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+// Here we are building the URL we need to query the database
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=Van Nuys, USA &appid=" + APIKey;
+
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function(response) {
+
+    // Log the queryURL
+    console.log(queryURL);
+
+    // Log the resulting object
+    console.log(response);
+
+    // Append to HTML
+    $(".city").html("<h5>" + response.name + " Weather Details</h5>");
+ 
+    // F temp conversion
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+    // add F-temp content to html
+    $(".tempF").text("Temperature (F): " + tempF.toFixed(2));
+    console.log("Temperature (F): " + tempF);
+    // Description var
+    var description = (response.weather[0].description);
+    console.log(description);
+    // description append
+    $(".description").text("Conditions: " + description)
+   
+  });
+
